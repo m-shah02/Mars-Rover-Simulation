40709f8b8ba35db8fad107993a81ec970999c613
m-shah02 <mansoorshah488@gmail.com>
2023-12-04 22:12:00 +0000

    Updated code in repo
    Looks visually better in the repository this way, minor touch ups done

--- Vehicle.java
+++ Vehicle.java
@@ -1,59 +1,54 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-class Vehicle extends Entity{
+
+class Vehicle extends Entity {
 	public boolean carryingSample;
-	
-	public Vehicle(Location l){
-		super(l);	
+
+	public Vehicle(Location l) {
+		super(l);
 		this.carryingSample = false;
 	}
 
-	public void act(Field f, Mothership m, ArrayList<Rock> rocksCollected)
-	{
-		//actCollaborative(f,m,rocksCollected);
-		actSimple(f,m,rocksCollected);
+	public void act(Field f, Mothership m, ArrayList<Rock> rocksCollected) {
+		actCollaborative(f, m, rocksCollected);
+		// actSimple(f,m,rocksCollected);
 	}
-	
-	public void moveTo(Field f, Location loc)
-	{
+
+	public void moveTo(Field f, Location loc) {
 		f.clearLocation(location);
-		f.place(this,loc);
+		f.place(this, loc);
 		this.setLocation(loc);
 	}
-	
-	public void actCollaborative(Field f, Mothership m, ArrayList<Rock> rocksCollected)
-	{
-		// If carrying sample & at base then drop samples
-		if (carryingSample && f.isNeighbourTo(this.location, Mothership.class))
-		{
-			carryingSample = false;
+
+	public void actCollaborative(Field f, Mothership m, ArrayList<Rock> rocksCollected) {
+
+		// if carrying a sample and at the base then drop sample
+		if (carryingSample && f.isNeighbourTo(this.location, Mothership.class)) {
+			this.carryingSample = false;
 			return;
 		}
-		
-		// If carrying a sample and not at the base then drop two crumbs and travel up gradient
-		if (carryingSample)
-		{
+
+		// if carrying a sample and not at the base then drop two crumbs and travel up
+		// gradient
+		if (carryingSample) {
 			f.dropCrumbs(this.location, 2);
-			ArrayList<Location> freeLocation = f.getAllfreeAdjacentLocations(location);
+			ArrayList<Location> freeLocations = f.getAllfreeAdjacentLocations(location);
 			Location bestLocation = location;
 			int highestSignal = f.getSignalStrength(location);
-			Iterator<Location> iter = freeLocation.iterator();
-			while(iter.hasNext())
-			{
+			Iterator<Location> iter = freeLocations.iterator();
+			while (iter.hasNext()) {
 				Location loc = iter.next();
-				if(f.getSignalStrength(loc) > highestSignal)
-				{
+				if (f.getSignalStrength(loc) > highestSignal) {
 					bestLocation = loc;
 					highestSignal = f.getSignalStrength(loc);
 				}
 			}
-			moveTo(f,bestLocation);
+			moveTo(f, bestLocation);
 			return;
 		}
-		
-		// If detect a sample then pick sample
-		if(f.isNeighbourTo(this.location, Rock.class))
-		{
+
+		// if detect a sample then pick sample
+		if (f.isNeighbourTo(this.location, Rock.class)) {
 			Location rockLoc = f.getNeighbour(this.location, Rock.class);
 			this.carryingSample = true;
 			Rock r = (Rock) f.getObjectAt(rockLoc);
@@ -61,70 +56,62 @@ class Vehicle extends Entity{
 			f.clearLocation(rockLoc);
 			return;
 		}
-		
-		// If sense crumbs then pick up one crumb and travel down gradient
-		if(f.getCrumbQuantityAt(this.location) > 0)
-		{
+
+		// if sense crumbs then pick up one crumb and travel down gradient
+		if (f.getCrumbQuantityAt(this.location) > 0) {
 			f.pickUpACrumb(this.location);
 			ArrayList<Location> freeLocations = f.getAllfreeAdjacentLocations(location);
 			Location bestLocation = location;
-			int lowestSignal = f.getSignalStrength(location)-f.getCrumbQuantityAt(location);
+			int lowestSignal = f.getSignalStrength(location) - f.getCrumbQuantityAt(location);
 			Iterator<Location> iter = freeLocations.iterator();
-			while(iter.hasNext())
-			{
+			while (iter.hasNext()) {
 				Location loc = iter.next();
 				// optimising travel down gradient to take crumbs into account (1c)
 				int strength = f.getSignalStrength(loc) - f.getCrumbQuantityAt(loc);
-				if(strength < lowestSignal)
-				{
+				if (strength < lowestSignal) {
 					bestLocation = loc;
 					lowestSignal = strength;
 				}
 			}
-			moveTo(f,bestLocation);
+			moveTo(f, bestLocation);
 			return;
 		}
-		
+
 		Location newLocation = f.freeAdjacentLocation(this.location);
-		if(newLocation != null)
-		{
-			// If true then move randomly
+		if (newLocation != null) {
+			// if true then move randomly
 			moveTo(f, newLocation);
 			return;
 		}
 	}
 
-	public void actSimple(Field f, Mothership m, ArrayList<Rock> rocksCollected){
-		// If carrying sample & at base then drop samples
-		if (carryingSample && f.isNeighbourTo(this.location, Mothership.class))
-		{
-			carryingSample = false;
+	public void actSimple(Field f, Mothership m, ArrayList<Rock> rocksCollected) {
+
+		// if carrying a sample and at the base then drop sample
+		if (carryingSample && f.isNeighbourTo(this.location, Mothership.class)) {
+			this.carryingSample = false;
 			return;
 		}
-		
-		// If carrying a sample and not at base then travel up gradient
-		if (carryingSample)
-		{
-			ArrayList<Location> freeLocation = f.getAllfreeAdjacentLocations(location);
+
+		// if carrying a sample and not at the base then travel up gradient
+		if (carryingSample) {
+			ArrayList<Location> freeLocations = f.getAllfreeAdjacentLocations(location);
 			Location bestLocation = location;
 			int highestSignal = f.getSignalStrength(location);
-			Iterator<Location> iter = freeLocation.iterator();
-			while(iter.hasNext())
-			{
+			Iterator<Location> iter = freeLocations.iterator();
+			while (iter.hasNext()) {
 				Location loc = iter.next();
-				if(f.getSignalStrength(loc) > highestSignal)
-				{
+				if (f.getSignalStrength(loc) > highestSignal) {
 					bestLocation = loc;
 					highestSignal = f.getSignalStrength(loc);
 				}
 			}
-			moveTo(f,bestLocation);
+			moveTo(f, bestLocation);
 			return;
 		}
-		
-		// If detect a sample then pick sample
-		if(f.isNeighbourTo(this.location, Rock.class))
-		{
+
+		// if detect a sample then pick sample
+		if (f.isNeighbourTo(this.location, Rock.class)) {
 			Location rockLoc = f.getNeighbour(this.location, Rock.class);
 			this.carryingSample = true;
 			Rock r = (Rock) f.getObjectAt(rockLoc);
@@ -132,11 +119,10 @@ class Vehicle extends Entity{
 			f.clearLocation(rockLoc);
 			return;
 		}
-		
+
 		Location newLocation = f.freeAdjacentLocation(this.location);
-		if(newLocation != null)
-		{
-			// If true then move randomly
+		if (newLocation != null) {
+			// if true then move randomly
 			moveTo(f, newLocation);
 			return;
 		}
